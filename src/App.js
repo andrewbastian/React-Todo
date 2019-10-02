f6dff5983abc62c2e976e6bd25a213fb7fb35575
andrewbastian <andrew.bastian@gmail.com>
2019-10-01 18:33:47 -0700

    Fist comit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import React from 'react';
-
+import TodoForm from './components/TodoComponents/TodoForm'
+import TodoList from './components/TodoComponents/TodoList'
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+const listItems = [
+  {
+  data: 'Finish this project',
+  id: 123,
+  doneX: false
+},
+{
+  data: 'Take a nap',
+  id: 124,
+  completed: false
+}
+]
+
+  constructor() {
+    super()
+    this.state={
+      neme: 'Andrew',
+      toDo: listItems,
+      display: listItems
+    }
+  }
+addData = dataName =>{
+  const newData = {}
+}
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-      </div>
+
+
+
+        <h1>Todo List</h1>
+        <TodoList/>
+        <h2>Add Item</h2>
+        <TodoForm />
+
+
+
+    </div>
+
     );
   }
 }
